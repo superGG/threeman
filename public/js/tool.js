03e1a48950e9986510d33d96c47b0188e41435e6
superGG <459714770@qq.com>
2017-09-01 18:11:25 +0800

    1

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,4 +1,4 @@
-var baseUrl = "http://1120.77.246.32:1234";
+var baseUrl = "http://120.77.246.32:1234";
 var serverUrl = baseUrl + "/api/";
 var socketUrl = "http://120.77.246.32:1234";
 var testUrl = "http://120.77.246.32:1234";
